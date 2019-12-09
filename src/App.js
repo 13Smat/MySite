fe5e0b499ecb7ad7a746b923fc9f1f6d3ca85731
Andy Silver <andy@vannoppen.co>
2019-12-09 16:39:47 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <div className="App">
+        <header className="App-header">
+          <p>Hello World</p>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
