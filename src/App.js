f228ff2b471dfb18a485f831e8532a94d1c42c4b
Andy Silver <andy@vannoppen.co>
2019-12-09 22:42:05 +0000

    Some styling

--- src/App.js
+++ src/App.js
@@ -3,62 +3,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="background">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <div className="App">
-        <header className="App-header">
-          <p>Hello World</p>
-        </header>
+    <div className="App">
+      <div className="background">
+        {Array(49)
+          .fill()
+          .map(_ => {
+            return <span></span>;
+          })}
       </div>
+      <header className="App-header">
+        <h1>Full Stack Developer located in Morganton, NC</h1>
+        <h2>Andy Silver</h2>
+      </header>
+      <section>
+        <h3>About Me</h3>
+        <p>THIs is AbOuT Me</p>
+      </section>
+      <section>
+        <a href="https://vannoppen.co">Vannoppen</a>
+      </section>
+      <footer className="App-footer">Whatever Text I want</footer>
     </div>
   );
 }
