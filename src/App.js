98fd61e71bcfd57c63a04a70ca55bf282e9b5a32
Andy Silver <andy@vannoppen.co>
2019-12-10 02:43:21 +0000

    More style adjustment

--- src/App.js
+++ src/App.js
@@ -17,10 +17,14 @@ function App() {
       </header>
       <section>
         <h3>About Me</h3>
-        <p>THIs is AbOuT Me</p>
+        <p>
+        I am a <strong>WCU Computer Science Graduate</strong> currently working at VanNoppen Marketing as a <strong>full stack developer</strong>. 
+        I create and assist in creating complex <strong>custom software applications</strong> for our clients website. I spend my free time doing small projects like this one so I
+        can continue to expand my knowledge in the <strong>Web Development Industry</strong>. I also have experience developing in <strong>Python, Java, C, JavaScript, and PosgreSQL.</strong>
+        </p>
       </section>
       <section>
-        <a href="https://vannoppen.co">Vannoppen</a>
+        <a href="https://vannoppen.co">VanNoppen</a>
       </section>
       <footer className="App-footer">Whatever Text I want</footer>
     </div>
